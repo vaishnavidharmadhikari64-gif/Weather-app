2e3081d317029c5846b2340b0b46f044be129965
vaishnavidharmadhikari64-gif <vaishnavidharmadhikari64@gmail.com>
2026-01-22 17:10:57 +0530

    Update WeatherCacheAppApplicationTests.java

--- src/test/java/com/metamorph/weather/WeatherCacheAppApplicationTests.java
+++ src/test/java/com/metamorph/weather/WeatherCacheAppApplicationTests.java
@@ -1,13 +1,29 @@
 package com.metamorph.weather;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class WeatherCacheAppApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private MockMvc mockMvc;
 
+    @Test
+    void contextLoads() {
+        // Ensures Spring context loads
+    }
+
+    @Test
+    void testGetWeatherEndpoint() throws Exception {
+        mockMvc.perform(get("/weather?city=Pune"))
+                .andExpect(status().isOk());
+    }
 }
